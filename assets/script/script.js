55f1756269285a52b4ed7bad1790fb7ac1377921
pablo de la cruz <pablodlc@gmail.com>
2021-05-07 20:32:53 -0700

    can search by city and get current data

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,19 @@
-const key = require("./key.js");
 const formEl = document.querySelector("#user-form");
 const cityEl = document.querySelector("#citysearch");
 
-const searchCity = function (city) {
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + key.apiKey;
-    fetch(apiUrl)
+// var apiUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&appid=' + apiKey + '&units=imperial';
+
+
+const latLon = function (city) {
+    let apiUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=829eec9644d40ff393c122e2b06045e8";
+    fetch(apiUrl1)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
+                    const { lat, lon } = data.coord;
+                    console.log(lat);
+                    console.log(lon);
+                    oneCall(lat, lon);
                 });
             } else {
                 alert('Error: City Not Found');
@@ -21,13 +26,57 @@ const searchCity = function (city) {
 
 };
 
+const oneCall = function (lat, lon) {
+    let apiUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&appid=829eec9644d40ff393c122e2b06045e8&units=imperial';
+    fetch(apiUrl2)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+
+                    $("#city").html(city);
+                    $("#temp").html(data.current.temp + "&deg;F");
+                    $("#wind").html(data.current.wind_speed + " MPH");
+                    $("#humid").html(data.current.humidity + "&percnt;");
+                    let uvi = data.current.uvi;
+                    $("#uvi").html(uvi);
+                    uviBg(uvi);
+                });
+            } else {
+                alert('Error: City Not Found');
+            }
+        })
+        .catch(function (error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to Open Weather Map");
+        });
+
+};
+
+const uviBg = function (uvi) {
+    if (uvi > 10) {
+        $("#uvi").css("background-color", "#EE82EE")
+    }
+    if (uvi > 7 && data.current.uvi < 11) {
+        $("#uvi").css("background-color", "red")
+    }
+    if (uvi > 5 && data.current.uvi < 8) {
+        $("#uvi").css("background-color", "orange")
+    }
+    if (uvi > 4 && data.current.uvi < 6) {
+        $("#uvi").css("background-color", "yellow")
+    }
+    if (uvi < 3) {
+        $("#uvi").css("background-color", "green")
+    }
+}
+
 
 const searchHandler = function (event) {
     event.preventDefault();
     let city = cityEl.value.trim();
 
     if (city) {
-        searchCity(city);
+        latLon(city);
         cityEl.value = "";
     }
     else {
